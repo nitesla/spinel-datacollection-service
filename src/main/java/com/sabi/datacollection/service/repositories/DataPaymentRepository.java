0d670b5819aae71667d9632c6ff62af3557a2419
toluadesola <t.toluadesola@gmail.com>
2022-04-25 08:19:24 +0100

    added services for wallet and payment

---
+++ src/main/java/com/sabi/datacollection/service/repositories/DataPaymentRepository.java
@@ -0,0 +1,24 @@
+package com.sabi.datacollection.service.repositories;
+
+import com.sabi.datacollection.core.models.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Map;
+
+public interface DataPaymentRepository extends JpaRepository<Payment, Long> {
+
+    @Query(value = "SELECT * FROM Payment WHERE " +
+            "((:paymentReference IS NULL) OR (:paymentReference IS NOT NULL AND Payment.paymentReference = :paymentReference)) " +
+            "AND ((:reference IS NULL) OR (:reference IS NOT NULL AND Payment.reference = :reference)) " +
+            "AND ((:status IS NULL) OR (:status IS NOT NULL AND Payment.status = :status)) " +
+            "AND ((:paymentMethod IS NULL) OR (:paymentMethod IS NOT NULL AND Payment.status = :paymentMethod))", nativeQuery = true)
+    Page<Payment> findAll(@Param("paymentReference") String paymentReference,
+                           @Param("reference") String reference,
+                           @Param("status") String status,
+                           @Param("paymentMethod") Integer paymentMethod,
+                           Pageable pageable);
+}
