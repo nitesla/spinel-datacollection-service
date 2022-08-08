392a7bdd2cdd9c0941571623daf68c8f218096b8
Kenoly <kelechi.a@spinel.consulting>
2022-08-08 16:19:54 +0100

    Further Refactoring

--- src/main/java/com/spinel/datacollection/service/repositories/UserBankRepository.java
+++ src/main/java/com/spinel/datacollection/service/repositories/UserBankRepository.java
@@ -1,15 +1,16 @@
-package com.sabi.datacollection.service.repositories;
+package com.spinel.datacollection.service.repositories;
 
 import com.sabi.datacollection.core.models.UserBank;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserBankRepository extends JpaRepository<UserBank, Long> {
 
     @Query("SELECT o FROM UserBank o WHERE ((:userId IS NULL) OR (:userId IS NOT NULL AND o.userId = userId)) " +
